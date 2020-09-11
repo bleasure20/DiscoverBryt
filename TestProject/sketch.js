e6c54411d80420c2365f99310f2255c878e85c24
Brytni M Leasure <leasureb@msu.edu>
2020-09-10 20:07:22 -0400

    1 - canvas created 2 - background color 3 - rectangle overlay 4 - multiple lines under rectangle

--- TestProject/sketch.js
+++ TestProject/sketch.js
@@ -1,15 +1,35 @@
-
 function setup() {
-  // put setup code here
-createCanvas(400,200);
-background(200,10,120);
 
+  createCanvas(500, 500);
 }
 
 function draw() {
-  //Put draw code here
-  fill(200,0,0);
-  circle(100,100,70);
+
+  background(245, 66, 233);
+  fill(126, 56, 224)
+
+
+  stroke(126, 56, 224);
+  point(width * 0.5, height * 0.5);
+  point(width * 0.5, height * 0.25);
+
+
+  stroke(0, 153, 255);
+  line(0, height * .2, width, height * .5);
+
+
+    stroke(56, 224, 222);
+    line(0, height * .3, width, height * .8);
+
+
+    stroke(56, 224, 222);
+    line(0, height *.8 , width, height * .8);
+
+
+  stroke(126, 56, 224);
+  rect(width * 0.25, height * 0.1, width * 0.5, height * 0.8);
+
+
 
 
 }
