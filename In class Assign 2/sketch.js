17029177838c8a51f66f79cdee382a07e32ae649
Brytni M Leasure <leasureb@msu.edu>
2020-09-17 15:12:49 -0400

    Created Ball that bounces without exiting screen and click to move in random place

--- In class Assign 2/sketch.js
+++ In class Assign 2/sketch.js
@@ -1,16 +1,52 @@
+
+let xpos = 500;
+let ypos = 0;
+let xballspeed = 3;
+let yballspeed = 4;
+
 function setup() {
 // Create a full screen canvas
 createCanvas(windowWidth, windowHeight);
+
 }
+
+
 function draw() {
-background(255,66,233);
+background(242, 174, 233);
 //Place circle in the center of the screen
-circle(mouseX,mouseY,50);
-rect(mouseX+50,mouseY+50,40)
-fill(mouseY,mouseX,255);
+circle(xpos,ypos,100);
+fill(145, 235, 224);
 
 
+xpos = xpos + xballspeed;
+ypos = ypos + yballspeed;
 
+
+//Chcecl fpr x position
+//Check when ball crosses right side of canvas
+if(xpos > width){
+  xballspeed = -xballspeed;
+
+
+}
+
+if(xpos  < 0){
+  xballspeed = -xballspeed;
+}
+//check when ball crosses left side of canvas
+if(ypos > height){
+  yballspeed = -yballspeed;
+
+}
+
+  if(ypos < 0){
+    yballspeed = -yballspeed;
+  }
+
+if( mouseIsPressed & dist(mouseX,mouseY,xpos,ypos)<50){
+  xpos = random(0,width);
+  ypos = random(0,height);
+}
 
 
 }
