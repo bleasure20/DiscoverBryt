bbc894e52e50a2e0901e2591299359cec5b4a6be
Brytni M Leasure <leasureb@msu.edu>
2020-09-24 11:47:44 -0400

    Added NewAssignment folder

---
+++ In class Assign 3/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+// Create a full screen canvas
+createCanvas(windowWidth, windowHeight);
+}
+function draw() {
+background(255,66,233);
+//Place circle in the center of the screen
+circle(mouseX,mouseY,50);
+rect(mouseX+50,mouseY+50,40)
+fill(mouseY,mouseX,255);
+
+
+
+
+
+}
