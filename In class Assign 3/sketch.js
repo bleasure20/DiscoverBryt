62d3c298fd917f584a0f4fbb2fa5eeb544fabe64
Brytni M Leasure <leasureb@msu.edu>
2020-09-24 14:45:14 -0400

    Added multiple circles and rectangles to make a visual sketch

--- In class Assign 3/sketch.js
+++ In class Assign 3/sketch.js
@@ -1,14 +1,53 @@
 function setup() {
 // Create a full screen canvas
 createCanvas(windowWidth, windowHeight);
+background(242, 174, 233);
+
+
+
+
 }
 function draw() {
-background(255,66,233);
-//Place circle in the center of the screen
-circle(mouseX,mouseY,50);
-rect(mouseX+50,mouseY+50,40)
-fill(mouseY,mouseX,255);
 
+  for(let i = 0; i < width; i = i +20){
+      fill(199, 96, 240);
+      rect(500,i,100);
+
+      fill(96, 213, 240);
+      circle(500,i,40);
+
+      fill(199, 96, 240);
+      rect(400,i,100);
+
+      fill(96, 213, 240);
+      circle(300,i,40);
+
+      circle(600,i,40);
+
+      fill(199, 96, 240);
+      rect(500,i,100);
+
+      fill(133, 255, 249);
+      circle(500,i,40);
+
+      fill(133, 255, 249);
+      circle(500,i,30);
+
+      fill(247, 82, 214);
+      rect(200,i,200);
+
+      fill(96, 213, 240);
+      circle(200,i,40);
+
+
+      fill(247, 82, 214);
+      rect(800,i,200);
+
+      fill(96, 213, 240);
+      circle(600,i,40);
+
+
+}
 
 
 
